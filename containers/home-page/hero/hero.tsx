1a3bdaa08057c14072662bb8c8aab72a79577af9
xuan vuong <lou1svuong.dev@gmail.com>
2024-07-23 16:40:59 +0700

    feat: Add HeroText component to display dynamic text in the hero section

---
+++ containers/home-page/hero/hero.tsx
@@ -0,0 +1,9 @@
+export default function HeroText() {
+  return (
+    <div className="sm:text-6xl  xl:text-8xl katibeh-regular w-full">
+      <h2 className="">Dear builders,</h2>
+      <h2 className="lg:ml-20">Don’t ask what APTOS can do for you</h2>
+      <h2 className="lg:ml-40">Ask what you can do for APTOS</h2>
+    </div>
+  );
+}
