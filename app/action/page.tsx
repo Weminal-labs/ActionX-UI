35622f32634cd33fe693a3465d2134233750d93f
xuan vuong <lou1svuong.dev@gmail.com>
2024-07-23 23:14:47 +0700

    refactor: Update Action component in page.tsx for responsive design

--- app/action/page.tsx
+++ app/action/page.tsx
@@ -17,12 +17,23 @@ export default function Action() {
   return (
     <>
       <GridBackground>
-        <div className="w-[55%] h-[9%]">
-          <PlaceholdersAndVanishInput
-            placeholders={placeholders}
-            onChange={handleChange}
-            onSubmit={onSubmit}
-          />
+        <div className="w-full h-full  hidden md:block">
+          <div className="flex justify-center w-full h-full items-center">
+            <div className="w-[55%] h-[9%]">
+              <PlaceholdersAndVanishInput
+                placeholders={placeholders}
+                onChange={handleChange}
+                onSubmit={onSubmit}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="md:hidden text-center">
+          <p className="font-medium text-[#B4E380] inline-block bg-black px-[0.375rem] rounded-full ">
+            Soon
+          </p>
+          <p className="font-semibold">Mobile currently unavailable</p>
+          <p className="text-gray-700">Please open this page on desktop</p>
         </div>
       </GridBackground>
     </>
