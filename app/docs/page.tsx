4cf0a9d5a1c43dd733f8eec64de55a1cdb4c2681
xuan vuong <lou1svuong.dev@gmail.com>
2024-07-25 07:26:43 +0700

    feat: Add DocsPage component for displaying documentation page

---
+++ app/docs/page.tsx
@@ -0,0 +1,9 @@
+export default function DocsPage() {
+  return (
+    <>
+      <div className="flex items-center justify-between text-center w-full h-screen">
+        <div className="w-full">is comming soon...</div>
+      </div>
+    </>
+  );
+}
