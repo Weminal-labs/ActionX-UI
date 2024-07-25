31bb818e50b44eca66ff17ed41fc4bc643c5840c
xuan vuong <lou1svuong.dev@gmail.com>
2024-07-25 07:27:38 +0700

    refactor: Update header component to use correct imports and dom

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,26 +1,25 @@
 "use client";
 import { useState } from "react";
-import { ModeToggle } from "@/components/toggle-theme";
-import { Button } from "@/components/ui/button";
 import LogoActionx from "@/public/image/logo-actionx";
 import RightArrow from "@/public/image/rightArrow";
 import Link from "next/link";
 import "@aptos-labs/wallet-adapter-ant-design/dist/index.css";
 import { WalletSelector } from "@/components/ui/WalletSelector";
+import { ModeToggle } from "@/components/ui/toggle-theme";
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <header className="flex items-center justify-between w-full fixed top-0 z-10 px-4 lg:px-16 py-4 bg-white lg:bg-transparent">
       <Link href={"/"}>
-        <div className="flex items-center relative">
+        <span className="flex items-center relative">
           <p className="text-2xl lg:text-3xl font-bold bg-logoGradientDark bg-clip-text text-transparent">
             Action
           </p>
-          <div className="absolute ml-[90%] mt-[25%]">
+          <span className="absolute ml-[90%] mt-[25%]">
             <LogoActionx height={100} width={100} />
-          </div>
-        </div>
+          </span>
+        </span>
       </Link>
 
       <div className="md:hidden">
@@ -37,12 +36,15 @@ export default function Header() {
           isOpen ? "flex" : "hidden"
         } absolute top-full left-0 right-0 bg-white md:bg-transparent md:static`}
       >
-        <Link href={"/docs"}>
-          <div className="flex items-center gap-2 px-4 py-2 md:p-0">
+        <div className="flex items-center gap-2 px-4 py-2 md:p-0">
+          <Link
+            href={"/docs"}
+            className="flex items-center gap-2 font-semibold"
+          >
             <RightArrow />
-            <p className="font-semibold">Docs X</p>
-          </div>
-        </Link>
+            Docs X
+          </Link>
+        </div>
         <WalletSelector />
         <ModeToggle />
       </nav>
