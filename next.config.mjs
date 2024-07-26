a1f877ffadf591fae79500bcf323de62d43a9118
xuan vuong <lou1svuong.dev@gmail.com>
2024-07-27 00:14:59 +0700

    chore : config IMAGE of nextjs for img tester

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.viblo.asia",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 export default nextConfig;
